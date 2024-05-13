f2981b7c6937d2780d8d3802228fcb0ffafa81d7
alidu28 <mugabejunior5@gmail.com>
2024-05-13 01:56:57 +0000

    "Mon_May commit"

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,15 +2,16 @@
 import React from 'react'
 import "./Hero.css"
 import profile_img from "../../assets/profile_img.svg"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div id='home' className='hero'>
         <img src={profile_img} alt="" />
         <h1><span>I`m Mohammed Alidu,</span> software developer based in Ghana.</h1>
         <p>I am a software developer from Ho, Ghana with a passion for coding.</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect with me</div>
+            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
             <div className="hero-resume">My resume</div>
         </div>
     </div>
